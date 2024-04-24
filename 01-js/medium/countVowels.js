bbf65fb5a3b4dabc2558548dafd2f22a9aa50f00
Pranay R <rps2579@gmail.com>
2024-04-24 10:12:12 +0530

    Solve 01-js/count vowels

--- 01-js/medium/countVowels.js
+++ 01-js/medium/countVowels.js
@@ -6,7 +6,27 @@
 */
 
 function countVowels(str) {
-    // Your code here
+  // return simple_soln(str);
+  return using_js_methods_soln(str);
+}
+
+function using_js_methods_soln(str) {
+  let vowels = 'aeiou';
+  return str.toLowerCase().split('')
+            .filter((char) => vowels.includes(char))
+            .length;
+}
+
+function simple_soln(str) {
+  str = str.toLowerCase();
+  let vowels = 'aeiou';
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (vowels.includes(str[i])) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = countVowels;
